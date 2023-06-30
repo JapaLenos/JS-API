68f1dc1b38a018d47966f97ef30b954a048bf047
Pauline <plouis@esrifrance.fr>
2023-06-30 09:47:42 +0200

    Add files via upload

---
+++ Jardins-Parisiens/app/step2-addJSONLayer-main.js
@@ -0,0 +1,52 @@
+require([
+        "esri/Map",
+        "esri/Basemap",
+        "esri/views/SceneView",
+        "esri/layers/GeoJSONLayer",
+        //"esri/layers/CSVLayer",
+          
+      ], 
+        
+        (Map, Basemap, SceneView ,GeoJSONLayer,/*CSVLayer*/) => {
+          
+          
+        const map = new Map({
+          basemap: new Basemap({
+            portalItem: {
+              id: "0560e29930dc4d5ebeb58c635c0909c9" // References the 3D Topographic Basemap
+            }
+          })
+        });
+        
+          const view = new SceneView({
+          container: "viewDiv",
+          map: map,
+          camera: {
+            position: {
+              longitude: 2.3122219,
+              latitude: 48.846614,
+              z: 1620.71497
+            },
+            heading: 80,
+            tilt: 50
+          } 
+        });
+          
+        view.when(() => {
+            
+            const arbresRemarquables = new GeoJSONLayer({
+            url: "/app/arbresremarquablesparis.geojson",
+            screenSizePerspectiveEnabled: false,
+            });
+            
+            map.add(arbresRemarquables);
+            
+/*            const arbresRemarquables = new CSVLayer({
+            url: "/app/arbresremarquablesparis.csv",
+            screenSizePerspectiveEnabled: false,
+            });
+            
+            map.add(arbresRemarquables);*/
+            
+        });
+});
